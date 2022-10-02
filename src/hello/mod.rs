3bf33f8ac19fc5d8849af5b81246591173e029cf
Kimmo Palin <kimmo.palin@helsinki.fi>
2022-10-02 09:21:10 +0000

    Question mark operator!

--- src/hello/mod.rs
+++ src/hello/mod.rs
@@ -1,3 +1,21 @@
 pub fn doprint() {
     println!("Apparently this works");
 }
+
+use std::fs::File;
+
+pub fn open_hello_file() -> File {
+    File::open("hello.txt").unwrap()
+}
+
+pub fn open_hello_file2() -> File {
+    File::open("hello.txt").expect("Ei onnaa")
+}
+use std::io;
+use std::io::Read;
+
+pub fn read_line(f: &mut File) -> Result<String, io::Error> {
+    let mut s = String::new();
+    f.read_to_string(&mut s)?; // Return Err if needed
+    Ok(s)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
 mod hello;
 
-use std::fs::File;
-use std::io::ErrorKind;
-
-fn open_hello_file() -> File {
-    match File::open("hello.txt") {
-        Ok(x) => x,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => panic!("No hello!"),
-            other_error => panic!("{:?}", other_error),
-        },
-    }
-}
-
 fn main() {
     crate::hello::doprint();
     println!("Hello, world!");
 
-    let f = open_hello_file();
-    panic!("Good bye, cruel world!");
+    let mut f = hello::open_hello_file2();
+    let s = hello::read_line(&mut f).unwrap();
+    panic!("Good bye, cruel world! '{}' {:?}", s.trim(), f);
 }
