8dbfdefd3b149c5286944b5e6e365811adf4371f
Kimmo Palin <kimmo.palin@helsinki.fi>
2022-10-02 17:45:38 +0000

    Traits etc.

--- src/hello/mod.rs
+++ src/hello/mod.rs
@@ -19,3 +19,26 @@ pub fn read_line(f: &mut File) -> Result<String, io::Error> {
     f.read_to_string(&mut s)?; // Return Err if needed
     Ok(s)
 }
+
+// Lifetimes  (Return value has the lifetime of the shortest lifetime of x and y)
+pub fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+use std::fmt::Display;
+// Chapter 10: Generics and lifetimes:
+pub fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,15 @@ fn main() {
 
     let mut f = hello::open_hello_file2();
     let s = hello::read_line(&mut f).unwrap();
-    panic!("Good bye, cruel world! '{}' {:?}", s.trim(), f);
+    println!("Good bye, cruel world! '{}' {:?}", s.trim(), f);
+
+    let a = String::from("Big");
+    let longger_ab;
+
+    let b = String::from("Smaller");
+
+    longger_ab = hello::longest(a.as_str(), b.as_str());
+
+    println!("Big or smaller: {:?}", longger_ab);
+    hello::longest_with_an_announcement(&a, &b, "Julistus!");
 }
