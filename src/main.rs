3469ad5e81c348633218c8fdf65ff0c529563126
Kimmo Palin <kimmo.palin@helsinki.fi>
2022-10-02 08:44:53 +0000

    Open file and error check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 mod hello;
 
+use std::fs::File;
+
+fn open_hello_file() -> File {
+    match File::open("hello.txt") {
+        Ok(x) => x,
+        Err(x) => panic!("{}", x),
+    }
+}
+
 fn main() {
     crate::hello::doprint();
     println!("Hello, world!");
+
+    let f = open_hello_file();
+    panic!("Good bye, cruel world!");
 }
